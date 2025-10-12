8eb40191cfaad38ee79ed4a58a146a435dc8bb7c
Renan Blasques <renanblasques@gmail.com>
2025-10-12 18:46:04 -0300

    Implementação do componente Inscrição

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,12 +3,14 @@ import EvaluationCriteria from "./components/EvaluationCriteria";
 import PrizesSection from "./components/PrizesSection";
 import Maps from "./components/Maps";
 import Sponsors from "./components/sponsors";
+import Subscription from "./components/Subscription";
 
 export default function Home() {
   return (
     <div className="font-roboto min-h-screen">
       <EvaluationCriteria />
       <PrizesSection />
+      <Subscription />
       <Maps />
       <Sponsors />
     </div>
